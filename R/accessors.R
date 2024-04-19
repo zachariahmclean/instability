c04780f19795b9a5d08374006a2670b6672762bc
zachariahmclean <zachariah.louis.mclean@gmail.com>
2024-04-19 16:44:21 -0400

    added citation and updated a few default function values

--- R/accessors.R
+++ R/accessors.R
@@ -708,7 +708,7 @@ add_metadata <- function(fragments_list,
 #' This function identifies main alleles within each fragment in a list of fragments.
 #'
 #' @param fragments_list A list of fragment objects containing peak data.
-#' @param number_of_peaks_to_return Number of main peaks to be returned for each fragment. Default is 2.
+#' @param number_of_peaks_to_return Number of main peaks to be returned for each fragment. Must either be 1 or 2 if a normal sized allele is also to be found.
 #' @param peak_region_size_gap_threshold Gap threshold for identifying peak regions. The peak_region_size_gap_threshold is a parameter used to determine the maximum allowed gap between peak sizes within a peak region. Adjusting this parameter affects the size range of peaks that can be grouped together in a region. A smaller value makes it more stringent, while a larger value groups peaks with greater size differences, leading to broader peak regions that may encompass wider size ranges.
 #' @param peak_region_height_threshold_multiplier Multiplier for the peak height threshold. The peak_region_height_threshold_multiplier parameter allows adjusting the threshold for identifying peak regions based on peak heights. Increasing this multiplier value will result in higher thresholds, making it more stringent to consider peaks as part of a peak region. Conversely, reducing the multiplier value will make the criteria less strict, potentially leading to more peaks being grouped into peak regions. It's important to note that this parameter's optimal value depends on the characteristics of the data and the specific analysis goals. Choosing an appropriate value for this parameter can help in accurately identifying meaningful peak regions in the data.
 #'
@@ -735,7 +735,7 @@ add_metadata <- function(fragments_list,
 #'   peak_region_height_threshold_multiplier = 1)
 
 find_alleles <- function(fragments_list,
-                         number_of_peaks_to_return = 2,
+                         number_of_peaks_to_return = 1,
                          peak_region_size_gap_threshold = 6,
                          peak_region_height_threshold_multiplier = 1){
 
